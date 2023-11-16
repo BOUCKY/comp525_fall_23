f51d2b0e5b2770f32c0a6aaf4d955c1187f92e08
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-11-16 18:18:06 -0500

    Completed lab 5.

--- labs/lab5/parser.py
+++ labs/lab5/parser.py
@@ -18,9 +18,16 @@ class RamenStats:
         """
         self.ramen_set = []
         with open(self.file_name, 'r', encoding='UTF-8') as fin:
-            lines = fin.readlines()
+            lines = fin.readlines()[1:]
             # Do something with the lines in order to fill self.ramen_set
             # with useful data
+            for line in lines:
+                line = line.strip()
+                line = line.split(',')
+                line[5] = float(line[5])
+                self.ramen_set.append(line)
+        # print(self.ramen_set)
+
 
     def get_average_for_all(self):
         """
@@ -28,7 +35,10 @@ class RamenStats:
         :return: the average for ratings for the given country
         :rtype: float
         """
-        pass
+        total_rating = 0
+        for ramen in self.ramen_set:
+            total_rating += ramen[5]
+        return total_rating / len(self.ramen_set)
 
     def get_average_for_country(self, country):
         """
@@ -38,7 +48,13 @@ class RamenStats:
         :return: the average for ratings for the given country
         :rtype: float
         """
-        pass
+        total_rating = 0
+        country_ratings = 0
+        for ramen in self.ramen_set:
+            if ramen[4] == country:
+                country_ratings += 1
+                total_rating += ramen[5]
+        return total_rating / country_ratings
 
     def get_percent_of_variety_that_include_the_word_ramen(self):
         """
@@ -46,7 +62,11 @@ class RamenStats:
         :return: the percentage of ramen variety that include the word "ramen"
         :rtype: float
         """
-        pass
+        country_ratings = 0
+        for ramen in self.ramen_set:
+            if 'Ramen' in ramen[2]:
+                country_ratings += 1
+        return country_ratings / len(self.ramen_set)
 
     def get_ramen_by_country(self):
         """
@@ -56,22 +76,28 @@ class RamenStats:
             ex: {'USA': [[2580, 'New Touch', "T's Restaurant', 'Cup', 'etc]]
         :rtype: dictionary
         """
-        pass
+        data = {}
+        for ramen in self.ramen_set:
+            country = ramen[4]
+            if country not in data:
+                data[country] = []
+            data[country].append(ramen)
+        return data
 
 
 if __name__ == "__main__":
     stats = RamenStats()
 
-    # average all expected to be ???
+    # # average all expected to be ???
     print(f"Average All: {stats.get_average_for_all():.2f}")
 
-    # average USA expected to be ???
+    # # average USA expected to be ???
     print(f"Average USA: {stats.get_average_for_country('USA'):.2f}")
 
-    # Num ramen with "ramen" in their variety expected to be ???
+    # # Num ramen with "ramen" in their variety expected to be ???
     percent_ramen = stats.get_percent_of_variety_that_include_the_word_ramen()
     print(f"Variety % ramen: {percent_ramen:.2f}")
 
-    # Num ramen for Japan expected to be ???
+    # # Num ramen for Japan expected to be ???
     ramen_categorized = stats.get_ramen_by_country()
     print(f"Num ramen for Japan: {len(ramen_categorized['Japan'])}")
